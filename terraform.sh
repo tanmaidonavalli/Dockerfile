cd80b5480f8159d5bac0f08e973b7bcc75b978a9
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-04 19:18:36 -0700

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,17 @@
+cd /root/TFCode/TF_Files
+$terraform --version
+
+
+
+#Intialize the folder
+$terraform init -no-color -upgrade=true 
+
+#Run config-lint
+$config-lint -terraform TF_Files
+
+$terraform plan -no-color -out=tfplan | tee plan.output
+
+
+      $terraform apply -no-color tfplan
+
+        
