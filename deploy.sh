8c7e9bb8a5db5b952ca8fc45052e545f8f69fb03
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-06 09:55:26 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t tfimage:v1 .
-docker run -it -d --name tcont1 tfimage:v1
-docker exec -it tfcontainer2 bash -c 'cd /root/TFCode/TF_Files && terraform init && terraform plan && terraform apply && docker stop tfcontainer2 && docker rm tfcontainer2 && docker rmi tfimage:v1 ; exec "${SHELL:-sh}"'
+docker run -it -d --name tfcontainer tfimage:v1
+docker exec -it tfcontainer bash -c 'cd /root/TFCode/TF_Files && terraform init && terraform plan && terraform apply && docker stop tfcontainer && docker rm tfcontainer && docker rmi tfimage:v1 ; exec "${SHELL:-sh}"'
