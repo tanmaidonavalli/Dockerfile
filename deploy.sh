b103d7da31d34207488289d3f1eb882237b3c994
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-10 18:47:25 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ docker exec -it tfcontainer1 bash -c 'cd /root/TFCode/TF_Files &&
         chmod 755 config.sh &&
 
         ./config.sh &&
-        terraform plan -var 'aws_access_key=AKIAWHX3VKYV66Q7E7WO' -var 'aws_secret_key=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' &&
+        terraform plan &&
         terraform apply &&
         exit ; exec "${SHELL:-sh}"'
 docker stop tfcontainer1
