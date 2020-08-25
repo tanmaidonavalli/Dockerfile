0005745957afcc99bd6e6289a18d55e6e406aea5
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-25 08:14:34 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 docker build -t tfimage:latest .
 docker run -it -d --name tfcontainer1 tfimage:latest
 docker exec -it tfcontainer1 bash -c 'cd /root/TFCode/TF_Files &&
-        terraform init  &&
-        config-lint 
+        
+        terraform init --backend-config="access_key=xxx" --backend-config="secret_key=xxx" &&
+        chmod 755 con.sh && ./con.sh &&
+        terraform workspace new "app1-dev" &&
         terraform plan &&
         terraform apply &&
         exit ; exec "${SHELL:-sh}"'
